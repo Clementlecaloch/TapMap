951e3ca7e509b93cb93daa424cc4ab6d4c944533
Lucas <45669808+Lulutorus@users.noreply.github.com>
2022-11-20 21:06:19 -0500

    map

--- app/src/main/java/com/example/tapmap/MainActivity.java
+++ app/src/main/java/com/example/tapmap/MainActivity.java
@@ -4,11 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.mapbox.maps.MapView;
+import com.mapbox.maps.Style;
+import com.mapbox.maps.plugin.annotation.AnnotationManager;
+import com.mapbox.maps.viewannotation.ViewAnnotationManager;
+
 public class MainActivity extends AppCompatActivity {
+
+    MapView mapView = null;
+    ViewAnnotationManager vam;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mapView = findViewById(R.id.mapView);
+        mapView.getMapboxMap().loadStyleUri(Style.SATELLITE_STREETS);
+        mapView.getViewAnnotationManager();
     }
 }
