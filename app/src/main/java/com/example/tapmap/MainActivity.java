dcdc4df477a4bfa3544ad34272a3ef0aa444e224
ClementLeCaloch <clemlecaloch@orange.fr>
2022-11-20 23:28:53 -0500

    Nav bar and fragments

--- app/src/main/java/com/example/tapmap/MainActivity.java
+++ app/src/main/java/com/example/tapmap/MainActivity.java
@@ -1,25 +1,55 @@
 package com.example.tapmap;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 
-import com.mapbox.maps.MapView;
-import com.mapbox.maps.Style;
-import com.mapbox.maps.plugin.annotation.AnnotationManager;
-import com.mapbox.maps.viewannotation.ViewAnnotationManager;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
-    MapView mapView = null;
-    ViewAnnotationManager vam;
+    AddFragment addFragment = new AddFragment();
+    FilterFragment filterFragment = new FilterFragment();
+    ShareFragment shareFragment = new ShareFragment();
+    AccountFragment accountFragment = new AccountFragment();
+    MapFragment mapFragment = new MapFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mapView = findViewById(R.id.mapView);
-        mapView.getMapboxMap().loadStyleUri(Style.SATELLITE_STREETS);
-        mapView.getViewAnnotationManager();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.map);
+    }
+
+    @SuppressLint("LongLogTag")
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.map:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, mapFragment).commit();
+                return true;
+            case R.id.add:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, addFragment).commit();
+                return true;
+            case R.id.share:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, shareFragment).commit();
+                return true;
+            case R.id.account:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, accountFragment).commit();
+                return true;
+            case R.id.filter:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, filterFragment).commit();
+                return true;
+        }
+        return false;
     }
 }
